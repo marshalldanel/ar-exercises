6cb03eb2ee2f5d0031a414e8a4465c2df1927f57
marshalldanel <marshalldanel@gmail.com>
2017-08-02 00:41:40 +0000

    Commit solution

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,39 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
+Surrey = Store.new name: "Surrey", annual_revenue: 224_000, mens_apparel: false, womens_apparel: true
+Whistler = Store.new name: "Whistler", annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false
+Yaletown = Store.new name: "Yaletown", annual_revenue: 430_000, mens_apparel: true, womens_apparel: true
+
+Surrey.save!
+Whistler.save!
+Yaletown.save!
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |s|
+  puts s.name
+  puts s.annual_revenue
+end
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1_000_000)
+
+@womens_stores.each do |s|
+  puts s.name
+  puts s.annual_revenue
+end
+
+
+
+
+
+
 # Your code goes here ...
+
+# 1. Borrowing and modifying the code from Exercise one, create 3 more stores:
+#   * Surrey (annual_revenue of 224000, carries women's apparel only)
+#   * Whistler (annual_revenue of 1900000 carries men's apparel only)
+#   * Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+# 2. Using the `where` class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable `@mens_stores`.
+# 3. Loop through each of these stores and output their name and annual revenue on each line.
+# 4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
