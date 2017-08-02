1f2eab4194b7a40ada9e9ca5958f9f1384624ce8
marshalldanel <marshalldanel@gmail.com>
2017-08-02 00:41:16 +0000

    Cleaned up number style

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,9 +3,9 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-Burnaby = Store.new name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true
-Richmond = Store.new name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true
-Gastown = Store.new name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false
+Burnaby = Store.new name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true
+Richmond = Store.new name: 'Richmond', annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true
+Gastown = Store.new name: 'Gastown', annual_revenue: 190_000, mens_apparel: true, womens_apparel: false
 
 Burnaby.save!
 Richmond.save!
