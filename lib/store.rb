c03ed51a917068757d0c67062c343e85f00248f7
Marshall Danel <marshalldanel@gmail.com>
2017-08-01 19:23:54 -0700

    Added validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :include_apparel
+
+
+  def include_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, ": must include one apparel")
+      errors.add(:womens_apparel, ": must include one apparel")
+    end
+  end
 end
