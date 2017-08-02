455d5de90153f9575b12e6e73b8c954a3e204c15
marshalldanel <marshalldanel@gmail.com>
2017-08-02 00:10:49 +0000

    Commit solutions

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+puts @store1.name
+
+@store2 = Store.find(2)
+puts @store2.name
+
+@store1.name = 'Burnabyyy'
+@store1.save!
+puts @store1.name
